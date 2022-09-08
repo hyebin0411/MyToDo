344de4acdad30029afef7198491c9534cba5ea97
hyebin0411 <4444ssome@gmail.com>
2022-09-08 13:24:03 +0900

    navbar + sidebar 시도 중

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import router from './router'
-import BootstrapVue3 from 'bootstrap-vue-3'
+import { BootstrapVue3, IconsPlugin } from 'bootstrap-vue-3'
 
 // Optional, since every component import their Bootstrap functionality
 // the following line is not necessary
@@ -10,9 +10,11 @@ import BootstrapVue3 from 'bootstrap-vue-3'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 //createApp(App).mount('#app')
 const app = createApp(App)
 app.use(BootstrapVue3)
 app.use(router)
+app.use(IconsPlugin)
 app.mount('#app')
