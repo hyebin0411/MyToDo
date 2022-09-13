a53635101a2777b298021299f5aa450a4146ad4e
hyebin0411 <4444ssome@gmail.com>
2022-09-13 15:47:19 +0900

    month page component 추가 및 side-bar transition 시도

--- src/main.js
+++ src/main.js
@@ -12,9 +12,12 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
+import jQuery from 'jquery'
+
 //createApp(App).mount('#app')
 const app = createApp(App)
 app.use(BootstrapVue3)
 app.use(router)
 app.use(IconsPlugin)
+global.$ = jQuery
 app.mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  pluginOptions: {
+    
+  }
 })
 module.exports = {
   chainWebpack: config => {
     config.resolve.alias
       .set('@', path.resolve(__dirname, 'src/'))
+  },
+
+  pluginOptions: {
+    vuetify: {}
   }
 }
