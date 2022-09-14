c235c4e21f759dc8e2832a47854015eb6a0131f7
hyebin0411 <4444ssome@gmail.com>
2022-09-14 15:02:41 +0900

    매일당근 오늘당근 타이틀 추가

---
+++ src/components/TodoEverydayTask/TodoEverydayTitle.vue
@@ -0,0 +1,30 @@
+<!-- 아이콘 + "매일 당근" +  -->
+<template>
+    <header class="header1">
+        <i class="bi bi-arrow-through-heart fs-2"></i>
+        <p class="header__date">매일 당근</p>
+    </header>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss">
+.header1{
+    background-color: whitesmoke;
+    display: flex;
+    // justify-content: space-between;
+    height : 50px;
+    margin-top: 30px;
+} 
+i{
+    width: 30px;
+    height: 30px;
+    margin-top: 3px;
+    margin-left: 3px;
+    margin-right: 3px;
+}
+</style>
--- src/pages/MonthPage.vue
+++ src/pages/MonthPage.vue
@@ -5,15 +5,26 @@
         <TodoNavbar />
 
         <h3>MonthPage 입니다. gg</h3>
+
+        <TodoEverydayTitle />
+
+        <TodoList /> <!--임시 리스트-->
+
+        <TodoTodayTitle />
+
+        <TodoList />
     </div>
 </template>
 
 <script>
 import TodoHeader from '@/components/TodoHeader.vue';
 import TodoNavbar from '@/components/TodoNavbar.vue';
+import TodoList from '@/components/TodoList.vue';
+import TodoTodayTitle from '@/components/TodoTodayTask/TodoTodayTitle.vue';
+import TodoEverydayTitle from '@/components/TodoEverydayTask/TodoEverydayTitle.vue';
 
 export default {
-    components: { TodoHeader, TodoNavbar  },
+    components: { TodoHeader, TodoNavbar, TodoList, TodoTodayTitle, TodoEverydayTitle  },
     methods : {
         ClickIcon : function(){
             console.log('ClickIcon() :: ');
